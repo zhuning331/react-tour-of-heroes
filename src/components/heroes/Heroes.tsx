c9bf35c829389c86070c583e20ced80420f49640
Jhu-Ning Chang <zhuning331@gmail.com>
2021-10-20 17:51:36 +0800

    加入hero interface，檢查hero state型別是否正確

--- src/components/heroes/Heroes.tsx
+++ src/components/heroes/Heroes.tsx
@@ -1,11 +1,19 @@
-import { useState } from 'react';
+import { useState, Fragment } from 'react';
 import SubTitle from '../../styles/SubTitle';
+import Hero from '../../types/hero';
 
 const Heroes = () => {
-    const [hero] = useState('Windstorm');
+    const [hero] = useState<Hero>({
+        id: 1,
+        name: 'Windstorm'
+    });
 
     return (
-        <SubTitle>{hero}</SubTitle>
+        <Fragment>
+            <SubTitle>{hero.name.toUpperCase()} Details</SubTitle>
+            <div><span>id: </span>{hero.id}</div>
+            <div><span>name: </span>{hero.name}</div>
+        </Fragment>
     )
 }
 

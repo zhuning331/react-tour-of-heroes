6a6840fab2d0efd53ce04a07a7767b4822c86ee0
Jhu-Ning Chang <zhuning331@gmail.com>
2021-10-20 17:21:48 +0800

    修正styled title和sub title寫法，讓引入時更簡潔

--- src/styles/SubTitle.ts
+++ src/styles/SubTitle.ts
@@ -1,7 +1,9 @@
 import styled from 'styled-components';
 
-export const SubTitle = styled.h2`
+const SubTitle = styled.h2`
     color: #444;
     font-family: Arial, Helvetica, sans-serif;
     font-weight: lighter;
 `
+
+export default SubTitle;
--- src/styles/Title.ts
+++ src/styles/Title.ts
@@ -1,7 +1,9 @@
 import styled from 'styled-components';
 
-export const Title = styled.h1`
+const Title = styled.h1`
     color: #369;
     font-family: Arial, Helvetica, sans-serif;
     font-size: 250%;
 `
+
+export default Title;

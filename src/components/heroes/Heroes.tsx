912de8172ca08b691a7e8eedfe667a7f7011083b
Jhu-Ning Chang <zhuning331@gmail.com>
2021-10-20 18:38:09 +0800

    完成列出英雄列表，並用styled-component改寫CSS

--- src/components/heroes/Heroes.tsx
+++ src/components/heroes/Heroes.tsx
@@ -1,17 +1,26 @@
 import { useState, Fragment } from 'react';
 import SubTitle from '../../styles/SubTitle';
-import HeroInput from '../../styles/HeroInput';
+import { HeroInput, HeroList, HeroItem, HeroBadge } from '../../styles/Hero';
 import Hero from '../../types/hero';
+import HEROES from '../../mock-heroes';
 
 const Heroes = () => {
     const [hero, setHero] = useState<Hero>({
         id: 1,
         name: 'Windstorm'
     });
+    const [heroes] = useState<Hero[]>(HEROES);
 
     return (
         <Fragment>
             <SubTitle>{hero.name.toUpperCase()} Details</SubTitle>
+            <HeroList>
+                {heroes.map(hero =>
+                    <HeroItem>
+                        <HeroBadge>{hero.id}</HeroBadge> {hero.name}
+                    </HeroItem>)
+                }
+            </HeroList>
             <div><span>id: </span>{hero.id}</div>
             <div>
                 <label htmlFor="name">Hero name: </label>
--- src/styles/HeroInput.ts
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-const HeroInput = styled.input`
-    padding: .5rem;
-`
-
-export default HeroInput;
